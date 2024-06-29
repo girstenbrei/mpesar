92ac4d974931ade2b4e4bbd5e68e6fd131d131f4
Christoph Girstenbrei <Christoph.Girstenbrei@gmail.com>
2024-06-29 20:12:20 +0200

    Zitat Felix: das sollte funktionieren

--- mainframe/src/bin/mainframe.rs
+++ mainframe/src/bin/mainframe.rs
@@ -1,6 +1,9 @@
 use axum::{body::Bytes, extract::State, http::StatusCode, routing::post, Router};
 use miette::{IntoDiagnostic, Result};
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use serde::Deserialize;
+use serde_json::from_slice;
+use sqlx::{postgres::PgPoolOptions, FromRow, PgPool};
+//use rust_decimal::Decimal
 
 #[derive(Debug, Clone)]
 struct AppState {
@@ -11,7 +14,7 @@ struct AppState {
 async fn main() -> Result<()> {
     let db = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:test@localhost/mainframe")
+        .connect("postgres://postgres:test@localhost:5432/mainframe")
         .await
         .into_diagnostic()?;
 
@@ -29,18 +32,38 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn handler(State(state): State<AppState>, body: Bytes) -> Result<String, StatusCode> {
+#[derive(Deserialize)]
+struct RequestBody {
+    account_id: i32,
+    ussd_code: String,
+}
 
+#[derive(FromRow)]
+struct Account {
+    account_id: i32,
+    //balance: Decimal
+}
+
+
+async fn handler(State(state): State<AppState>, body: Bytes) -> Result<String, StatusCode> {
     println!("Received body: {}", String::from_utf8_lossy(&body));
 
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&state.db).await
+    let request_body: RequestBody = from_slice(&body).map_err(|e| StatusCode::BAD_REQUEST)?;
+
+    let account_id: i32 = request_body.account_id;
+    let ussd_code = request_body.ussd_code;
+
+    println!("Received Account ID {}", account_id);
+    println!("Received USSD Code {}", ussd_code);
+
+    let account: Account = sqlx::query_as("SELECT * FROM mainframe.accounts WHERE account_id = $1")
+        .bind(account_id)
+        .fetch_one(&state.db)
+        .await
         .map_err(|e| {
             println!("Failed fetching data from db: {}", e);
             StatusCode::INTERNAL_SERVER_ERROR
-        }
-        )?;
+        })?;
 
-    Ok(format!("Fetched {}", row.0))
+    Ok(format!("Received Account"))
 }
